5642a530ee98708f65d01ef88a916b9d1db87f55
Ma-rk <kim.mark.dev@gmail.com>
2024-09-09 19:55:41 +0900

    app.py 추가

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def redirect_to_komatbang():
+    return redirect('https://www.instagram.com/komatbang/')
+
+
+@app.route('/health')
+def health_check():
+    return 'healthy'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
