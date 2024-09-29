6903737d1623161fedecfb7d800e5f1296032ed5
Ma-rk <kim.mark.dev@gmail.com>
2024-09-29 21:34:31 +0900

    http/https 및 서브도메인별로 처리하는 컨트롤러를 구분함

--- app.py
+++ app.py
@@ -29,6 +29,30 @@ def redirect_to_komatbang():
     return redirect('https://www.instagram.com/komatbang/')
 
 
+@app.route('/http')
+def redirect_to_komatbang_http():
+    lg.info('http!!! redirecting to komatbang: https://www.instagram.com/komatbang/')
+    return redirect_to_komatbang()
+
+
+@app.route('/http-www')
+def redirect_to_komatbang_http_www():
+    lg.info('http-www!!! redirecting to komatbang: https://www.instagram.com/komatbang/')
+    return redirect_to_komatbang()
+
+
+@app.route('/https')
+def redirect_to_komatbang_https():
+    lg.info('http!!! redirecting to komatbang: https://www.instagram.com/komatbang/')
+    return redirect_to_komatbang()
+
+
+@app.route('/https-www')
+def redirect_to_komatbang_https_www():
+    lg.info('https-www!!! redirecting to komatbang: https://www.instagram.com/komatbang/')
+    return redirect_to_komatbang()
+
+
 @app.route('/health-http')
 def health_check_http():
     return 'healthy'
