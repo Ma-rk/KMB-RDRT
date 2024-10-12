f25d56807d247db1c7b6ab53505ee6e6e889be76
Ma-rk <kim.mark.dev@gmail.com>
2024-10-12 21:45:27 +0900

    print_request_log() 함수를 추출함

--- app.py
+++ app.py
@@ -91,11 +91,15 @@ def health_check_http():
     return 'healthy'
 
 
-@app.teardown_request
-def teardown_request(exception):
+def print_request_log():
     lg.info(f'[{g.request_id}][teardown_request] request: {request}')
     lg.info(f'[{g.request_id}][teardown_request] request.path: {request.path}')
     lg.info(f'[{g.request_id}][teardown_request] request.url: {request.url}')
     headers_data = dict(request.headers)
     formatted_headers = json.dumps(headers_data, indent=4, separators=(",", ": "), ensure_ascii=False)
     lg.info(f'[{g.request_id}][teardown_request] Headers Data:\n{formatted_headers}')
+
+
+@app.teardown_request
+def teardown_request(exception):
+    print_request_log()
